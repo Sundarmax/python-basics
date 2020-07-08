f48af0caaec6f0c041a0878a1f1b21cad11deba1
sundarmax <sundar.info22@gmail.com>
2020-07-08 23:09:07 +0530

    multiple inheritance update

--- protected.py
+++ protected.py
@@ -28,6 +28,33 @@ class Department(StudentProfile):
     # accessing protected member functions of super class 
         self._getStudentProfile()
 
-test = Department("sundar","21","934849834")
-test.getStudentName()
+"""
+ Desc:
+  Python program to demonstrate the diamond problem
+  (a.k.a. Multiple Inheritance)
+"""
+
+# Parent class 1
+class TeamMember(object):                   
+   def __init__(self, name, uid): 
+      self.name = name 
+      self.uid = uid 
+  
+# Parent class 2
+class Worker(object):                 
+   def __init__(self, pay, jobtitle): 
+      self.pay = pay 
+      self.jobtitle = jobtitle 
+  
+# Deriving a child class from the two parent classes
+class TeamLeader(TeamMember, Worker):         
+   def __init__(self, name, uid, pay, jobtitle, exp): 
+      self.exp = exp 
+      TeamMember.__init__(self, name, uid) 
+      Worker.__init__(self, pay, jobtitle)
+      print("Name: {}, Pay: {}, Exp: {}".format(self.name, self.pay, self.exp))
+
+TL = TeamLeader('Jake', 10001, 250000, 'Scrum Master', 5)
+#test = Department("sundar","21","934849834")
+#test.getStudentName()
 
